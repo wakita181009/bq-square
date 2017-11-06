46d3aea6e893990e8a844816703af51db18c0210
rororo12 <ro.syoyu@fourm.asia>
2017-11-06 18:40:01 +0900

    Fix handle 500 error

--- src/app.py
+++ src/app.py
@@ -60,7 +60,8 @@ app = webapp2.WSGIApplication([
 
     webapp2.Route('/getReport/<url_safe:([a-zA-z0-9_-]+)|>', GetReportHandler, name='getReport'),
     webapp2.Route('/getReportList', GetReportListHandler, name='getReportList'),
-    webapp2.Route('/getGlobalKeyValue/<key_name:([a-zA-z0-9_-]+)|>', GetGlobalKeyValueHandler, name='getGlobalKeyValue'),
+    webapp2.Route('/getGlobalKeyValue/<key_name:([a-zA-z0-9_-]+)|>', GetGlobalKeyValueHandler,
+                  name='getGlobalKeyValue'),
 
     webapp2.Route('/run_query/<query_id:([a-zA-z0-9_-]+)|>', RunQueryHandler, name='run_query'),
     webapp2.Route('/run_query/<query_id:([a-zA-z0-9_-]+)|>/<t:table|plot|chart|>', RunQueryHandler,
@@ -79,21 +80,7 @@ def handle_error(request, response, e):
     response.headers['Access-Control-Allow-Origin'] = CLIENT
     response.headers['Access-Control-Allow-Headers'] = 'Origin, X-Requested-With, Content-Type, Accept, Authorization'
     response.write(e)
-    response.set_status(e.code)
-
-
-def handle_server_error(request, response, e):
-    if isinstance(e, Exception):
-        logging.exception('[ERROR HANDLER] %s: %s' % (type(e).__name__, e.message))
-    else:
-        logging.exception('[ERROR HANDLER] %s' % e)
-
-    response.headers['Access-Control-Allow-Methods'] = 'POST, GET, PUT, DELETE, OPTIONS'
-    response.headers['Access-Control-Allow-Credentials'] = 'true'
-    response.headers['Access-Control-Allow-Origin'] = CLIENT
-    response.headers['Access-Control-Allow-Headers'] = 'Origin, X-Requested-With, Content-Type, Accept, Authorization'
-    response.write('Server error')
-    response.set_status(500)
+    response.set_status(getattr(e, 'code', 500))
 
 
 def handle_not_found_error(request, response, e):
@@ -113,6 +100,4 @@ def handle_not_found_error(request, response, e):
 app.error_handlers[403] = handle_error
 app.error_handlers[404] = handle_not_found_error
 app.error_handlers[400] = handle_error
-
-if PROD:
-    app.error_handlers[500] = handle_server_error
+app.error_handlers[500] = handle_error
