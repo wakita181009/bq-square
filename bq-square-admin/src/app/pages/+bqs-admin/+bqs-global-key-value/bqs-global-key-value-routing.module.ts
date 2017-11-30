f793c9a8e1033e7dd5854be3ec3577f765e97984
rororo12 <ro.syoyu@fourm.asia>
2017-11-30 14:22:35 +0900

    Fix UI

--- bq-square-admin/src/app/pages/+bqs-admin/+bqs-global-key-value/bqs-global-key-value-routing.module.ts
+++ bq-square-admin/src/app/pages/+bqs-admin/+bqs-global-key-value/bqs-global-key-value-routing.module.ts
@@ -1,30 +1,38 @@
 import {NgModule} from '@angular/core';
 import {Routes, RouterModule} from '@angular/router';
 
+import {BqsGlobalKeyValueComponent} from './bqs-global-key-value.component';
 import {BqsGlobalKeyListComponent} from './bqs-global-key-list/bqs-global-key-list.component';
 import {BqsGlobalKeyEditComponent} from './bqs-global-key-edit/bqs-global-key-edit.component';
 import {BqsGlobalKeyViewComponent} from './bqs-global-key-view/bqs-global-key-view.component';
 
+
 const routes: Routes = [
   {
     path: '',
-    component: BqsGlobalKeyListComponent
-  },
-  {
-    path: 'list',
-    component: BqsGlobalKeyListComponent
-  },
-  {
-    path: 'edit',
-    component: BqsGlobalKeyEditComponent
-  },
-  {
-    path: 'edit/:urlsafe',
-    component: BqsGlobalKeyEditComponent
-  },
-  {
-    path: ':urlsafe',
-    component: BqsGlobalKeyViewComponent
+    component: BqsGlobalKeyValueComponent,
+    children: [
+      {
+        path: '',
+        component: BqsGlobalKeyListComponent
+      },
+      {
+        path: 'list',
+        component: BqsGlobalKeyListComponent
+      },
+      {
+        path: 'edit',
+        component: BqsGlobalKeyEditComponent
+      },
+      {
+        path: 'edit/:urlsafe',
+        component: BqsGlobalKeyEditComponent
+      },
+      {
+        path: ':urlsafe',
+        component: BqsGlobalKeyViewComponent
+      }
+    ]
   }
 ];
 
--- bq-square-admin/src/app/pages/+bqs-admin/+bqs-global-key-value/bqs-global-key-value.module.ts
+++ bq-square-admin/src/app/pages/+bqs-admin/+bqs-global-key-value/bqs-global-key-value.module.ts
@@ -9,6 +9,7 @@ import {createModelReducer} from 'app/store/model/model.reducer';
 import {injectAsyncReducer} from 'app/store/root.reducer';
 
 import {BqsGlobalKeyValueRoutingModule} from './bqs-global-key-value-routing.module';
+import {BqsGlobalKeyValueComponent} from './bqs-global-key-value.component';
 import {BqsGlobalKeyListComponent} from './bqs-global-key-list/bqs-global-key-list.component';
 import {BqsGlobalKeyEditComponent} from './bqs-global-key-edit/bqs-global-key-edit.component';
 import {BqsGlobalKeyViewComponent} from './bqs-global-key-view/bqs-global-key-view.component';
@@ -19,7 +20,7 @@ import {BqsGlobalKeyViewComponent} from './bqs-global-key-view/bqs-global-key-vi
     SharedModule,
     BqsGlobalKeyValueRoutingModule
   ],
-  declarations: [BqsGlobalKeyListComponent, BqsGlobalKeyEditComponent, BqsGlobalKeyViewComponent]
+  declarations: [BqsGlobalKeyValueComponent, BqsGlobalKeyListComponent, BqsGlobalKeyEditComponent, BqsGlobalKeyViewComponent]
 })
 export class BqsGlobalKeyValueModule {
   constructor(private store: NgRedux<IAppState>) {
